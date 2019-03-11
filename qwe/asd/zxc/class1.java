0b7f6dcb67a1fb71408bbf96dc36b3b01917d23a
testns <44125335+testns@users.noreply.github.com>
2019-03-12 02:24:55 +0300

    Update class1.java

--- qwe/asd/zxc/class1.java
+++ qwe/asd/zxc/class1.java
@@ -8,8 +8,14 @@ import java.nio.file.Paths;
 
 public class FileTest {
 
-    public static void main(String[] args) {
-    
+    public static void main(String[] args) throws IOException {
+other        String content = "select * from student;";
+        final Path tempDirectory = Files.createDirectories(Paths.get("nikita715/plagiarism_test/testns"));
+        final Path tempFile = Files.createFile(Paths.get(tempDirectory.toString(), "script.sql"));
+        try (final FileOutputStream fileOutputStream = new FileOutputStream(tempFile.toFile())) {
+            fileOutputStream.write(content.getBytes());
+        }
+        Files.deleteIfExists(tempDirectory);
     }
 
 }
