68dae0deea0b4b61bd9627f019233df3ff16653a
Nikita Stepochkin <nikstepbox@gmail.com>
2019-03-13 20:36:34 +0300

    Update class1.java

--- qwe/asd/zxc/class1.java
+++ qwe/asd/zxc/class1.java
@@ -2,7 +2,7 @@ package files;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.nio.file.Files;//comment
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
