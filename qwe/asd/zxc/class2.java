0cbdca86e445e44e50ae9ba0d48c390dab0d173a
Nikita Stepochkin <nikstepbox@gmail.com>
2019-03-13 20:49:55 +0300

    Update class2.java

--- qwe/asd/zxc/class2.java
+++ qwe/asd/zxc/class2.java
@@ -4,6 +4,17 @@ public class FileTest {
 
     public static void main(String[] args) {
     
+        show(world);
+        System.out.println();
+        world = nextGen(world);
+        show(world);
+        Scanner s = new Scanner(System.in);
+        while(s.nextLine().length() == 0){
+            System.out.println();
+            world = nextGen(world);
+            show(world);
+            
+        }
     }
 
 }
