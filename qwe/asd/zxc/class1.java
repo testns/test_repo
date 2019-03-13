826a47aaab4a2cf2933d814e8c8c5cb754f3889f
testns2 <47989149+testns2@users.noreply.github.com>
2019-03-13 20:35:28 +0300

    Update class1.java

--- qwe/asd/zxc/class1.java
+++ qwe/asd/zxc/class1.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+//comment
 public class FileTest {
 
     public static void main(String[] args) {
