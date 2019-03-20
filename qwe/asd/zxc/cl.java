957b10944f7a193f5e7b67bdc881f1f22f915893
Nikita Stepochkin <nikstepbox@gmail.com>
2019-03-20 22:02:26 +0300

    Update cl.java

--- qwe/asd/zxc/cl.java
+++ qwe/asd/zxc/cl.java
@@ -3,7 +3,7 @@
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Scanner;//commj
+import java.util.Scanner;//commjh
 
 
 public class URLExpSimple {
