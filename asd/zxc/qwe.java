14cd096b1a00b26525e3915eb5cfc22ac430087a
Nikita Stepochkin <nikstepbox@gmail.com>
2019-05-22 21:38:54 +0300

    Update qwe.java

--- asd/zxc/qwe.java
+++ asd/zxc/qwe.java
@@ -7,7 +7,7 @@ public class ReverseNumber {
         while(num != 0) {
             int digit = num % 10;
             reversed = reversed * 10 + digit;
-            num /= 10;//ssdfdsfdfgdfgvcbcvbdfg
+            num /= 10;//ssdfdsfdfgdfgvcbcvbdfgkjhkjhkjh
             
         while(num != 0) {
             int digit = num % 10;
